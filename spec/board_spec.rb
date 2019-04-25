871ec33dab72ebb4b4b187347ea4bf940f3d6506
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-04-25 23:20:57 +0600

    allignment check

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,18 +1,18 @@
 require_relative '../lib/board.rb'
 
 RSpec.describe Board do
-    let(:board) {Board.new([1,2,3,4,5,6,7,8,9])}
-    
-    describe "#full?" do
-        it "checks if the whole 3x3 grid is full" do
-            expect(board.full?).to eql(false)
-        end
+  let(:board) {Board.new([1,2,3,4,5,6,7,8,9])}
+  
+  describe "#full?" do
+    it "checks if the whole 3x3 grid is full" do
+        expect(board.full?).to eql(false)
     end
+  end
 
-    describe "#place_taken?" do
-        it "checks if a specific grid is already filled" do
-            board.environ[3] = "X"
-            expect(board.place_taken?(3)).to eql(true)
-        end
+  describe "#place_taken?" do
+    it "checks if a specific grid is already filled" do
+        board.environ[3] = "X"
+        expect(board.place_taken?(3)).to eql(true)
     end
+  end
 end
